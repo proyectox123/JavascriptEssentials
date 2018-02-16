20be2ea5efbe81d7a2e6a923d5fbc225330b36a4
MHO <mauricio.hdez.o.10@gmail.com>
2018-02-15 19:21:46 -0600

    Array modification.

---
+++ section2/lesson18/myapp.js
@@ -0,0 +1,15 @@
+var array = [
+	"String",
+	100,
+	[ "embed", 200 ], 
+	{ car: "Ford" },
+	function(){ return "Drive"; }
+];
+
+//array.shift(); // Remove first element
+//array.pop(); // Remove last element
+//array.unshift(0.1, 0.2, 0.3, null); // Add elements at the top
+//array.push(0.1, 0.2, 0.3, null); // Add elements at the bottom
+//array.splice( 2 , 2); // Remove elements from a position
+//array.splice( 2 , 0, ...); // Added elements from a position
+//array.splice( 2 , 2, ...); // Replace elements from a position
