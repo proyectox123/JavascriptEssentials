fd156c9077d816234eae37aa65fc1c5cac5e532f
MHO <mauricio.hdez.o.10@gmail.com>
2018-02-20 17:55:10 -0600

    Inferred Globals & Scope.

---
+++ section2/lesson22/myapp.js
@@ -0,0 +1,34 @@
+var engine = {
+	maker: "Ford",
+	headGasket: {
+		pots:[
+			"piston1",
+			"piston2"
+		]
+	}
+}
+
+/*
+function runExpression(){
+	var a = 10;
+
+	function add(){
+		var engine = "String engine";
+		console.log(engine);
+	}
+
+	add();
+}
+*/
+
+function runExpression(){
+	var a = 10;
+
+	function add(){
+		test = "New string";
+		test2 = "New string";
+		test3 = "New string";
+	}
+
+	add();
+}
