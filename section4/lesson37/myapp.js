731b7d98eb732b999449df76d6c5f72900e5f20d
MHO <mauricio.hdez.o.10@gmail.com>
2018-03-01 16:43:52 -0600

    Final project.

---
+++ section4/lesson37/myapp.js
@@ -0,0 +1,17 @@
+let elements = document.getElementsByName('cssProperty');
+let div = document.getElementById('modify');
+
+function set(){
+	for(let index = 0; index < elements.length; index++){
+		let cssProperty = elements[index].getAttribute('id');
+		let cssValue = elements[index].value;
+
+		updateStyle(cssProperty, cssValue)
+	}
+}
+
+function updateStyle(cssProperty, cssValue){
+	div.style[ cssProperty ] = cssValue;
+}
+
+document.getElementById('set').addEventListener('click', set);
